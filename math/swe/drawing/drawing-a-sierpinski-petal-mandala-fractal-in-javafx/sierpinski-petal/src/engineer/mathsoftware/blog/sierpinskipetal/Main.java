8728904cead90d8905f5001a31abbcc667283f45
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-14 02:02:36 -0600

    Add "Hello World" JavaFX app

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/engineer/mathsoftware/blog/sierpinskipetal/Main.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/engineer/mathsoftware/blog/sierpinskipetal/Main.java
@@ -4,8 +4,40 @@
 
 package engineer.mathsoftware.blog.sierpinskipetal;
 
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private static final double WINDOW_WIDTH = 960.0;
+    private static final double WINDOW_HEIGHT = 640.0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    public Main() {
+        super();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        var root = new VBox();
+        var scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        var btn = new Button();
+
+        primaryStage.setTitle("Sierpinski Petal");
+
+        btn.setText("Hello World");
+        btn.setOnAction(actionEvent -> System.out.println("Hello World"));
+
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(btn);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
