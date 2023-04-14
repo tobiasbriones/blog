9cca77d9dfca0d0608669e58d0cf80b00a8fc3df
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-14 02:01:46 -0600

    Add module-info.java

---
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/module-info.java
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/blog
+
+module engineer.mathsoftware.blog.sierpinskipetal {
+    requires javafx.controls;
+    exports engineer.mathsoftware.blog.sierpinskipetal;
+}
