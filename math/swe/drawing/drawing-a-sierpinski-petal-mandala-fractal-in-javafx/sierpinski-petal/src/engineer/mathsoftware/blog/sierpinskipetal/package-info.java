f1bb0227344d7e87757d1fb22775707e680e2094
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-14 01:46:08 -0600

    Add copyright license header

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/engineer/mathsoftware/blog/sierpinskipetal/package-info.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/engineer/mathsoftware/blog/sierpinskipetal/package-info.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/blog
+
 /**
  * It creates a Sierpinski Petal mandala fractal drawing on JavaFX.
  *
