3b6b4b1a2793e4d78ac4dd7c3225f76e06131d7c
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-14 01:42:14 -0600

    Add package-info.java on sierpinskipetal

---
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/engineer/mathsoftware/blog/sierpinskipetal/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * It creates a Sierpinski Petal mandala fractal drawing on JavaFX.
+ *
+ * @see
+ * <a
+ *   href="https://blog.mathsoftware.engineer/drawing-a-sierpinski-petal-mandala-fractal-in-javafx"
+ * >
+ * Drawing a Sierpinski Petal Mandala Fractal in JavaFX | Blog |
+ * mathsoftware.engineer
+ * </a>
+ */
+package engineer.mathsoftware.blog.sierpinskipetal;
