cb939fb5f627bf957a4440b57678bb634d8fa7a7
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-25 21:04:12 -0600

    Implement base text to reset the playground

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/Playground.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/Playground.java
@@ -4,15 +4,20 @@
 
 package engineer.mathsoftware.blog.sierpinskipetal.app;
 
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
+import javafx.scene.text.TextAlignment;
 
 @SuppressWarnings("ALL")
 class Playground {
     final Canvas canvas;
     final GraphicsContext ctx;
     final double scale;
+    double opacity;
     String title;
 
     double width() { return canvas.getWidth() / scale; }
@@ -27,6 +32,7 @@ class Playground {
         this.canvas = canvas;
         this.ctx = canvas.getGraphicsContext2D();
         this.scale = scale;
+        this.opacity = 1.0;
         this.title = "Drawing a Flower";
     }
 
@@ -35,9 +41,46 @@ class Playground {
     }
 
     void reset() {
+        clean();
+    }
+
+    void clean() {
         var bgColor = Color.web("#fafafa");
 
+        ctx.setGlobalAlpha(1);
         ctx.setFill(bgColor);
         ctx.fillRect(0.0, 0.0, width(), height());
+        drawSource(title);
+        ctx.setGlobalAlpha(opacity);
+    }
+
+    void drawSource(String title) {
+        setDrawingText("medium", 24, VPos.BOTTOM);
+
+        ctx.fillText(
+            "BLOG | mathsoftware.engineer",
+            width() / 2,
+            height() - 32
+        );
+        ctx.fillText(
+            "Drawing a Sierpinski Petal Mandala Fractal in JavaFX",
+            width() / 2,
+            height() - 64
+        );
+
+        setDrawingText("extrabold", 36, VPos.BOTTOM);
+        ctx.fillText(title, width() / 2, height() - 96);
+    }
+
+    void setDrawingText(
+        String weight,
+        int size,
+        VPos baseline
+    ) {
+        ctx.setFill(Color.web("#212121"));
+        ctx.setTextAlign(TextAlignment.CENTER);
+        ctx.setTextBaseline(baseline);
+        ctx.setFontSmoothingType(FontSmoothingType.GRAY);
+        ctx.setFont(Font.font("poppins " + weight, size));
     }
 }
