d300bfd16558274e53de780532479f867a5d469a
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-25 18:13:54 -0600

    Add class Playground with basic impl

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/Playground.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/Playground.java
@@ -4,7 +4,40 @@
 
 package engineer.mathsoftware.blog.sierpinskipetal.app;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 @SuppressWarnings("ALL")
 class Playground {
+    final Canvas canvas;
+    final GraphicsContext ctx;
+    final double scale;
+    String title;
 
+    double width() { return canvas.getWidth() / scale; }
+
+    double height() { return canvas.getHeight() / scale; }
+
+    double cx() { return width() / 2; }
+
+    double cy() { return height() / 2; }
+
+    Playground(Canvas canvas, double scale) {
+        this.canvas = canvas;
+        this.ctx = canvas.getGraphicsContext2D();
+        this.scale = scale;
+        this.title = "Drawing a Flower";
+    }
+
+    void play() {
+        reset();
+    }
+
+    void reset() {
+        var bgColor = Color.web("#fafafa");
+
+        ctx.setFill(bgColor);
+        ctx.fillRect(0.0, 0.0, width(), height());
+    }
 }
