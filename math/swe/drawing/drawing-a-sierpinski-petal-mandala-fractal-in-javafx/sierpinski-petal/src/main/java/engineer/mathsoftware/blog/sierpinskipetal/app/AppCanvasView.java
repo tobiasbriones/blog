723ed61c609bd7dbd5d6c0b22b277a40987f1927
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-25 18:13:25 -0600

    Add class AppCanvasView with basic impl

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/AppCanvasView.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/AppCanvasView.java
@@ -4,6 +4,38 @@
 
 package engineer.mathsoftware.blog.sierpinskipetal.app;
 
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+
 class AppCanvasView {
-    AppCanvasView() {}
+    private static final double CANVAS_SCALE = 1.0;
+    private static final double CANVAS_WIDTH = 900.0 * CANVAS_SCALE;
+    private static final double CANVAS_HEIGHT = 900.0 * CANVAS_SCALE;
+    private static final Color bgColor = Color.web("#fafafa");
+    private final Canvas canvas;
+    private final AppController controller;
+
+    AppCanvasView(AppController controller) {
+        this.canvas = new Canvas();
+        this.controller = controller;
+    }
+
+    Node getNode() {
+        return canvas;
+    }
+
+    void init() {
+        canvas.setWidth(CANVAS_WIDTH);
+        canvas.setHeight(CANVAS_HEIGHT);
+
+        draw();
+    }
+
+    void draw() {
+        var ctx = canvas.getGraphicsContext2D();
+
+        ctx.setFill(bgColor);
+        ctx.fillRect(0.0, 0.0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
 }
