364be2397e2c95a7a7ecdf6656cddc4abfa3570a
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-26 20:41:14 -0600

    Create utility class Transformation
    It'll define the transformation stateful.

---
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/state/Transformation.java
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/blog
+
+package engineer.mathsoftware.blog.sierpinskipetal.state;
+
+public final class Transformation {
+    private Transformation() {}
+}
