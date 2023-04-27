4b5f938e78f1fae7007869ec62311f726fb786f9
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-26 20:30:58 -0600

    Create package state

---
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/state/package-info.java
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/blog
+
+/**
+ * Provides state implementations for application models.
+ */
+package engineer.mathsoftware.blog.sierpinskipetal.state;
