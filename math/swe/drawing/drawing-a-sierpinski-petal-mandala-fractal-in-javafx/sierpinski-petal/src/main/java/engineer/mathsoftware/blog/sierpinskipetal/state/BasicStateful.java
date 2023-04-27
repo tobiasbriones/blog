4197c64110e4a1794d5cb167cb77e265b28d7e44
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-26 20:48:25 -0600

    Implement Basic.Stateful on BasicStateful

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/state/BasicStateful.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/state/BasicStateful.java
@@ -4,6 +4,53 @@
 
 package engineer.mathsoftware.blog.sierpinskipetal.state;
 
-class BasicStateful {
-    BasicStateful() {}
+class BasicStateful implements Basic.Stateful {
+    private final Position.Stateful point;
+    private double scale;
+
+    BasicStateful() {
+        this.point = new Point();
+        this.scale = 1.0;
+    }
+
+    @Override
+    public double getScale() {
+        return scale;
+    }
+
+    @Override
+    public void setScale(double value) {
+        scale = value;
+    }
+
+    // implements PositionState.Stateful by Point //
+    @Override
+    public double getX() {
+        return point.getX();
+    }
+
+    @Override
+    public void setX(double value) {
+        point.setX(value);
+    }
+
+    @Override
+    public double getY() {
+        return point.getY();
+    }
+
+    @Override
+    public void setY(double value) {
+        point.setY(value);
+    }
+
+    @Override
+    public void translateX(double value) {
+        point.translateX(value);
+    }
+
+    @Override
+    public void translateY(double value) {
+        point.translateY(value);
+    }
 }
