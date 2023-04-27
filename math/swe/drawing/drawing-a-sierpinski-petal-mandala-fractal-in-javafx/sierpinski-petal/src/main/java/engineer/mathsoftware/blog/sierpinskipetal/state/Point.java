a9b89340d2f33fb30b226dac46b0163a07ee9062
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-04-26 20:39:56 -0600

    Implement Position.Stateful on Point

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/state/Point.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/state/Point.java
@@ -4,6 +4,42 @@
 
 package engineer.mathsoftware.blog.sierpinskipetal.state;
 
-class Point {
-    Point() {}
+class Point implements Position.Stateful {
+    private double x;
+    private double y;
+
+    Point() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(double value) {
+        x = value;
+    }
+
+    @Override
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(double value) {
+        y = value;
+    }
+
+    @Override
+    public void translateX(double value) {
+        x += value;
+    }
+
+    @Override
+    public void translateY(double value) {
+        y += value;
+    }
 }
