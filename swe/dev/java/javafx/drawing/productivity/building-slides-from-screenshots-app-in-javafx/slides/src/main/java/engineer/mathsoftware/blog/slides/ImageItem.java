c4dfd30a0ec99d3791e03b88973e2b3859877587
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-05-19 20:49:08 -0600

    Override methods hashCode and equals on record ImageItem
    The image should not be calculated, and the filename is enough in this case to match two ImageItem.

--- swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides/src/main/java/engineer/mathsoftware/blog/slides/ImageItem.java
+++ swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides/src/main/java/engineer/mathsoftware/blog/slides/ImageItem.java
@@ -6,7 +6,20 @@ package engineer.mathsoftware.blog.slides;
 
 import javafx.scene.image.Image;
 
+import java.util.Objects;
+
 public record ImageItem(
     String filename,
     Image image
-) {}
+) {
+    @Override
+    public int hashCode() {
+        return Objects.hash(filename);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof ImageItem imageItem)
+            && Objects.equals(filename, imageItem.filename());
+    }
+}
