22bf60540ad9cdb48fbb3e12f9896f2f6f4265c2
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-05-19 21:08:14 -0600

    Extend ListCell on ImageItemCell
    It designs the rendering for an item of the list of images.

--- swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides/src/main/java/engineer/mathsoftware/blog/slides/ImageItemCell.java
+++ swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides/src/main/java/engineer/mathsoftware/blog/slides/ImageItemCell.java
@@ -4,6 +4,121 @@
 
 package engineer.mathsoftware.blog.slides;
 
-class ImageItemCell {
-    ImageItemCell() {}
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.shape.Rectangle;
+
+class ImageItemCell extends ListCell<ImageItem> {
+    interface Listener {
+        void onDelete(ImageItem item);
+    }
+
+    private final Listener l;
+    private final HBox view;
+    private final ImageView imageView;
+    private final Label nameLabel;
+    private final Button deleteButton;
+    private final Tooltip tip;
+
+    ImageItemCell(Listener l) {
+        super();
+        this.l = l;
+        this.view = new HBox();
+        this.imageView = new ImageView();
+        this.nameLabel = new Label();
+        this.deleteButton = new Button();
+        this.tip = new Tooltip();
+
+        init();
+    }
+
+    @Override
+    protected void updateItem(ImageItem item, boolean empty) {
+        super.updateItem(item, empty);
+        if (item == null || empty) {
+            setText(null);
+            setGraphic(null);
+        }
+        else {
+            updateItem(item);
+        }
+    }
+
+    private void init() {
+        var imageParent = new HBox();
+
+        imageView.setFitWidth(128.0);
+        imageView.setFitHeight(64.0);
+        imageView.setPreserveRatio(true);
+
+        imageParent.setPrefWidth(144.0);
+        imageParent.setPrefHeight(96.0);
+        imageParent.setAlignment(Pos.CENTER);
+        imageParent.getChildren().add(imageView);
+
+        var maxNameLength = 20;
+
+        nameLabel
+            .textProperty()
+            .addListener((observable, oldValue, newValue) -> {
+                if (newValue.length() > maxNameLength) {
+                    var txt = newValue.substring(0, maxNameLength) + "...";
+                    nameLabel.setText(txt);
+                }
+            });
+
+        var deleteParent = new HBox();
+
+        deleteButton.setText("X");
+        deleteButton.setStyle("-fx-text-fill: #b00020;");
+
+        HBox.setHgrow(deleteParent, Priority.ALWAYS);
+        deleteParent.setSpacing(16.0);
+        deleteParent.setAlignment(Pos.CENTER_RIGHT);
+        deleteParent.getChildren().add(deleteButton);
+
+        Tooltip.install(view, tip);
+
+        view.setPrefHeight(96.0);
+        view.setAlignment(Pos.CENTER_LEFT);
+        view.setSpacing(16.0);
+        view.getChildren().addAll(imageParent, nameLabel, deleteParent);
+    }
+
+    private void updateItem(ImageItem item) {
+        nameLabel.setText(item.filename());
+        imageView.setImage(item.image());
+        imageView.setSmooth(true);
+
+        var boundingBox = imageView.getLayoutBounds();
+        var clip = new Rectangle(
+            boundingBox.getWidth(), boundingBox.getHeight()
+        );
+
+        clip.setArcWidth(8.0);
+        clip.setArcHeight(8.0);
+        imageView.setClip(clip);
+
+        deleteButton.setOnAction(event -> onDeleteButtonAction(item));
+        tip.setText(item.filename());
+
+        setGraphic(view);
+    }
+
+    private void onDeleteButtonAction(ImageItem item) {
+        var alert = new Alert(
+            Alert.AlertType.CONFIRMATION,
+            "Delete " + item.filename() + "?",
+            ButtonType.YES,
+            ButtonType.NO
+        );
+        alert.showAndWait();
+
+        if(alert.getResult() == ButtonType.YES) {
+            l.onDelete(item);
+        }
+    }
 }
