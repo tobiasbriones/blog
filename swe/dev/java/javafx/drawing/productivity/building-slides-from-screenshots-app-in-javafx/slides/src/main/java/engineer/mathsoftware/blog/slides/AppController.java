01ca978bc30e75a36bfafe458003a91c29fd8b58
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-05-19 21:00:11 -0600

    Init Button addButton with ic_add Graphic icon

--- swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides/src/main/java/engineer/mathsoftware/blog/slides/AppController.java
+++ swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides/src/main/java/engineer/mathsoftware/blog/slides/AppController.java
@@ -8,18 +8,27 @@ import engineer.mathsoftware.blog.slides.data.DataRepository;
 import engineer.mathsoftware.blog.slides.data.LocalDataRepository;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
+
+import java.util.Objects;
 
 public class AppController {
     private static final String DATA_ROOT = "data";
     private final DataRepository repository;
+    @FXML
+    private Button addButton;
 
     public AppController() {
         this.repository = new LocalDataRepository(DATA_ROOT);
     }
 
     @FXML
-    public void initialize() {}
+    public void initialize() {
+        initAddButton();
+    }
 
     @FXML
     private void onDragOver(DragEvent dragEvent) {}
@@ -35,4 +44,17 @@ public class AppController {
 
     @FXML
     private void onClearButtonAction() {}
+
+    private void initAddButton() {
+        var icAdd = new Image(
+            Objects.requireNonNull(
+                getClass().getResourceAsStream("/ic_add.png")
+            )
+        );
+        var addImageView = new ImageView(icAdd);
+
+        addImageView.setFitWidth(18.0);
+        addImageView.setFitHeight(18.0);
+        addButton.setGraphic(addImageView);
+    }
 }
