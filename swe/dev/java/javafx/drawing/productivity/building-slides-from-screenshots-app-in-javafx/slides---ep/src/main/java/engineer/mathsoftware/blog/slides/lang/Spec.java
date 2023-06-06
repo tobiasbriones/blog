63678e7264a75deee62180a933bc5322eaa58384
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-06-05 20:00:43 -0600

    Update utility class Spec with Java Keywords

--- swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides---ep/src/main/java/engineer/mathsoftware/blog/slides/lang/Spec.java
+++ swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides---ep/src/main/java/engineer/mathsoftware/blog/slides/lang/Spec.java
@@ -5,5 +5,62 @@
 package engineer.mathsoftware.blog.slides.lang;
 
 public final class Spec {
+    public static final class Java {
+        public enum Keyword {
+            Abstract,
+            Assert,
+            Boolean,
+            Break,
+            Byte,
+            Case,
+            Catch,
+            Char,
+            Class,
+            Const,
+            Continue,
+            Default,
+            Do,
+            Double,
+            Else,
+            Enum,
+            Extends,
+            Final,
+            Finally,
+            Float,
+            For,
+            Goto,
+            If,
+            Implements,
+            Import,
+            Instanceof,
+            Int,
+            Interface,
+            Long,
+            Native,
+            New,
+            Package,
+            Private,
+            Protected,
+            Public,
+            Record,
+            Return,
+            Sealed,
+            Short,
+            Static,
+            Strictfp,
+            Super,
+            Switch,
+            Synchronized,
+            This,
+            Throw,
+            Throws,
+            Transient,
+            Try,
+            Void,
+            Volatile,
+            While
+        }
+    }
+
     private Spec() {}
 }
