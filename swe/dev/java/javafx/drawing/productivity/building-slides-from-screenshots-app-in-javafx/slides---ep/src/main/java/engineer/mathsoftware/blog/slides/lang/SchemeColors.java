89bf418d512e5366d5c682a811072122837b1b1b
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-06-06 16:56:53 -0600

    Add utility class SchemeColors in package lang

---
+++ swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides---ep/src/main/java/engineer/mathsoftware/blog/slides/lang/SchemeColors.java
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/blog
+
+package engineer.mathsoftware.blog.slides.lang;
+
+import javafx.scene.paint.Color;
+
+public final class SchemeColors {
+    public static Color color(Element element) {
+        return switch (element.toEnum()) {
+            case Keyword -> Color.web("#7986cb");
+            case Symbol -> Color.web("#cc7832");
+            case Number -> Color.web("#0288d1");
+            case StringLiteral -> Color.web("#558b2f");
+            case Other -> Color.WHITE;
+        };
+    }
+
+    private SchemeColors() {}
+}
