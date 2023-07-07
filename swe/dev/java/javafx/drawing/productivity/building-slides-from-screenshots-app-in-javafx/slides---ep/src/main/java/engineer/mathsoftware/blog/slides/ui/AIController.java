7b1a4dbec492b366d419e9415c804965dd4fa33f
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-07-07 00:37:28 -0600

    Update class AIController with frontend AI integration logic

--- swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides---ep/src/main/java/engineer/mathsoftware/blog/slides/ui/AIController.java
+++ swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides---ep/src/main/java/engineer/mathsoftware/blog/slides/ui/AIController.java
@@ -4,12 +4,28 @@
 
 package engineer.mathsoftware.blog.slides.ui;
 
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Group;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.transform.Scale;
+
+import static engineer.mathsoftware.blog.slides.ai.SlideAI.OcrWordDetection;
 
 class AIController {
+    private final ObjectProperty<OcrWordDetection> wordDetectionProperty;
+    private Group group;
+    private SlideAIView aiView;
+    private Image slideDrawingSnapshot;
     private BackgroundStatus status;
 
     AIController() {
+        wordDetectionProperty = new SimpleObjectProperty<>();
+        group = null;
+        aiView = null;
+        slideDrawingSnapshot = null;
         status = null;
     }
 
@@ -17,9 +33,69 @@ class AIController {
         status = newStatus;
     }
 
-    void init(Group slideDrawing) {}
+    void init(Group slideDrawing) {
+        group = slideDrawing;
+        aiView = new SlideAIView();
+        slideDrawingSnapshot = null;
 
-    void onShowTextBoxes() {}
+        aiView.init(slideDrawing);
+        aiView.hide();
+        aiView.ocrWordDetectionProperty().bind(wordDetectionProperty);
+        loadSlideDrawingSnapshot();
+        loadOcr();
+    }
 
-    void onHideTextBoxes() {}
+    void onShowTextBoxes() {
+        aiView.show();
+    }
+
+    void onHideTextBoxes() {
+        aiView.hide();
+    }
+
+    private void loadSlideDrawingSnapshot() {
+        if (slideDrawingSnapshot == null) {
+            var params = new SnapshotParameters();
+            var invScaleX = 1.0 / group.getScaleX();
+            var invScaleY = 1.0 / group.getScaleY();
+
+            params.setTransform(new Scale(invScaleX, invScaleY));
+            slideDrawingSnapshot = group.snapshot(params, null);
+        }
+    }
+
+    private void loadOcr() {
+        clearOcr();
+        setStatusMsg("Loading AI...");
+
+        Thread.startVirtualThread(
+            () -> {
+                try {
+                    var det = OcrWordDetection.from(slideDrawingSnapshot);
+
+                    Platform.runLater(() -> {
+                        loadOcr(det);
+                        setStatusMsg("AI loaded");
+                    });
+                }
+                catch (RuntimeException e) {
+                    setStatusMsg(e.getMessage());
+                }
+            }
+        );
+    }
+
+    private void loadOcr(OcrWordDetection det) {
+        wordDetectionProperty.set(det);
+    }
+
+    private void clearOcr() {
+        wordDetectionProperty.set(null);
+    }
+
+    private void setStatusMsg(String msg) {
+        if (status != null) {
+            status.setStatus(msg);
+        }
+    }
 }
