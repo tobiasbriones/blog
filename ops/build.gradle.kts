b1e938e08f271a8ea2e095f21a6c8760de81b481
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-07-14 23:35:54 -0600

    Add arrow lib

--- ops/build.gradle.kts
+++ ops/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.arrow-kt:arrow-core:1.2.0")
+    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.0")
     testImplementation(kotlin("test"))
 }
 
