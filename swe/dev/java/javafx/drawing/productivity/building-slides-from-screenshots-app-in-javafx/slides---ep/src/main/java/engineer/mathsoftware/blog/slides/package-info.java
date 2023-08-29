e51cefd106ef2c1ce6834fe29261c8eca5569027
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-08-28 20:40:26 -0600

    Clean

--- swe/dev/java/javafx/drawing/productivity/building-slides-from-screenshots-app-in-javafx/slides---ep/src/main/java/engineer/mathsoftware/blog/slides/package-info.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Tobias Briones. All rights reserved.
-// SPDX-License-Identifier: BSD-3-Clause
-// This file is part of https://github.com/tobiasbriones/blog
-
-/**
- * Provides a desktop application that converts screenshots into professional
- * slides that tell a story.
- */
-package engineer.mathsoftware.blog.slides;
