faa800538c73555a34b4750897453bf67850be9b
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-09-29 23:20:45 -0600

    Comment smaller caption size
    Captions can be part of the animation itself to save space, so they will have this size then.

--- math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/Playground.java
+++ math/swe/drawing/drawing-a-sierpinski-petal-mandala-fractal-in-javafx/sierpinski-petal/src/main/java/engineer/mathsoftware/blog/sierpinskipetal/app/Playground.java
@@ -472,6 +472,12 @@ class Playground {
 
     sealed interface Caption {
         double TITLE_SIZE = 1.5;
+
+        // Use this smaller size for embeded captions (i.e. captions are part
+        // of the whole animation). If the font size is bigger (1.5) then the
+        // caption is separate from the animation.
+        // double TITLE_SIZE = 1.25;
+
         double ABSTRACT_SIZE = TITLE_SIZE * 1.5;
 
         record Title(String value) implements Caption {}
