cb9fda054dd0302301b009287e4a303222324081
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-10-10 17:19:05 -0600

    .

--- ops/src/main/kotlin/Dic.kt
+++ ops/src/main/kotlin/Dic.kt
@@ -3,20 +3,20 @@ import md.Dictionary
 val dic: Dictionary = Dictionary(
     uppercase = setOf(
         "bsd", "mit", "ep", "bi", "unah-vs", "ai", "ml", "swe",
-        "sar", "hn", "hn:", "mrm",
+        "sar", "hn", "hn:", "mrm", "fxml",
     ),
     composed = mapOf(
         "mathswe" to listOf("math", "swe"),
         "github" to listOf("git", "hub"),
         "gitlab" to listOf("git", "lab"),
         "youtube" to listOf("you", "tube"),
-        "" to listOf("", ""),
+        "listview" to listOf("list", "view"),
         "" to listOf("", ""),
     ),
     acronym = mapOf(
         "ddo" to "Data-Driven Organizations",
     ),
     custom = mapOf(
-        "intellij idea" to "IntelliJ IDEA"
+        "intellij idea" to "IntelliJ IDEA",
     )
 )
