3aa626fc037ceaf2e300e28a548b6e110b8dab8a
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-03 23:34:55 -0600

    Add code for first sections

--- cs/fp/math/geometry/design/draft/designing-the-angle-geometry-for-the-oriented-segment-of-canvas-play/angle-design/Main.hs
+++ cs/fp/math/geometry/design/draft/designing-the-angle-geometry-for-the-oriented-segment-of-canvas-play/angle-design/Main.hs
@@ -1,3 +1,46 @@
 -- Copyright (c) 2024 Tobias Briones. All rights reserved.
 -- SPDX-License-Identifier: BSD-3-Clause
 -- This file is part of https://github.com/tobiasbriones/blog
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Data.Maybe
+
+-- mod Angle
+newtype Angle = Angle Double
+  deriving (Show, Num)
+
+newtype Acute = Acute Angle -- (0-90)
+
+newtype Obtuse = Obtuse Angle -- (90-180)
+
+newtype ReflexObtuse = ReflexObtuse Angle -- (180-270)
+
+newtype ReflexAcute = ReflexAcute Angle -- (180-360)
+
+data QuadrantalAngle
+  = Zero
+  | Right
+  | Straight
+  | ReflexRight
+
+angle :: QuadrantalAngle -> Angle
+angle x = Angle $ case x of
+  Zero -> 0
+  Main.Right -> 90
+  Straight -> 180
+  ReflexRight -> 270
+
+data Quadrant
+  = QI
+  | QII
+  | QIII
+  | QIV
+
+data QuadrantAngle (q :: Quadrant) where
+  AngleI :: Acute -> QuadrantAngle 'QI
+  AngleII :: Obtuse -> QuadrantAngle 'QII
+  AngleIII :: ReflexObtuse -> QuadrantAngle 'QIII
+  AngleIV :: ReflexAcute -> QuadrantAngle 'QIV
