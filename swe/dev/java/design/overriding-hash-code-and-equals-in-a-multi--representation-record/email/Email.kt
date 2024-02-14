27c8ef5551e772e98cca457ef3df93759c2d3123
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-14 17:31:48 -0600

    Add functions for email local normalization

--- swe/dev/java/design/overriding-hash-code-and-equals-in-a-multi--representation-record/email/Email.kt
+++ swe/dev/java/design/overriding-hash-code-and-equals-in-a-multi--representation-record/email/Email.kt
@@ -1,3 +1,22 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This file is part of https://github.com/tobiasbriones/blog
+
+/**
+ * It removes the redundancy of an email local value containing dots (.).
+ */
+fun normalizedLocalDot(local: String): String =
+    local.replace(".", "")
+
+/**
+ * It removes the redundancy of an email local value containing a plus (+)
+ * symbol, by eliminating everything after the first + occurrence.
+ */
+fun normalizedLocalPlus(local: String): String =
+    local.takeWhile { char -> char != '+' }
+
+/**
+ * It normalizes the email local value by removing any redundancy.
+ */
+fun normalizedLocal(local: String): String =
+    normalizedLocalDot(normalizedLocalPlus(local))
