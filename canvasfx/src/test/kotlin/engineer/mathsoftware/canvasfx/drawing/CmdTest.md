ddbaa526780ef50f4c3a1002f5a9070ef2dca8b2
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-05-03 23:02:05 -0600

    .

--- canvasfx/src/test/kotlin/engineer/mathsoftware/canvasfx/drawing/CmdTest.md
+++ canvasfx/src/test/kotlin/engineer/mathsoftware/canvasfx/drawing/CmdTest.md
@@ -1,4 +1,1 @@
-`.\gradlew run --args="--bg=bg.png --profile-photo=profile.jpeg --heading=texsydo/@MVP -
--abstract='Powering **Textual System Documenting** with the new **Texsydo** MVP.' --footer='Integrates PR command.,Dev
-elops CoverPr FX.,Deploys CanvasFX.' --subheading='Web (SWAM GX)' --subdomain=subdomain.png"
-`
+`.\gradlew run --args="pr-cover --bg=bg.png --profile-photo=profile.jpeg --heading=texsydo/@MVP --abstract='Powering **Textual System Documenting** with the new **Texsydo** MVP.' --footer='Integrates PR command.,Develops CoverPr FX.,Deploys CanvasFX.' --subheading='Web (SWAM GX)' --subdomain=subdomain.png"`
--- canvasfx/src/test/kotlin/engineer/mathsoftware/canvasfx/drawing/CoverPrKtTest.kt
+++ canvasfx/src/test/kotlin/engineer/mathsoftware/canvasfx/drawing/CoverPrKtTest.kt
@@ -5,6 +5,9 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.file.Path
 
 
 class CoverPrKtTest : Application() {
@@ -38,5 +41,64 @@ class CoverPrKtTest : Application() {
 }
 
 fun main() {
-    Application.launch(CoverPrKtTest::class.java)
+//    Application.launch(CoverPrKtTest::class.java)
+    val preview =
+        """app pr-cover preview --bg=bg.png --profile-photo=profile.jpeg --heading=texsydo/@MVP --abstract="Powering **Textual System Documenting** with the new **Texsydo** MVP." --footer="Integrates PR command.,Develops CoverPr FX.,Deploys CanvasFX." --subheading="Web (SWAM GX)" --subdomain=subdomain.png"""
+    val command =
+        """app pr-cover --bg=bg.png --profile-photo=profile.jpeg --heading=texsydo/@MVP --abstract="Powering **Textual System Documenting** with the new **Texsydo** MVP." --footer="Integrates PR command.,Develops CoverPr FX.,Deploys CanvasFX." --subheading="Web (SWAM GX)" --subdomain=subdomain.png"""
+
+//    val o = runCommand(preview)
+    val o = runCommand(command)
+    println(o)
+}
+
+
+fun runCommand(
+    command: String,
+    workingDir: Path? = null,
+): String {
+    val winCommand = "cmd /c $command"
+    val processBuilder = ProcessBuilder(
+        *winCommand
+            .split("\\s+".toRegex())
+            .toTypedArray()
+    )
+
+    processBuilder.redirectErrorStream(false)
+
+    if (workingDir != null) {
+        processBuilder.directory(workingDir.toFile())
+    }
+
+    return try {
+        val process = processBuilder.start()
+        val reader = BufferedReader(InputStreamReader(process.inputStream))
+        val stdErrReader =
+            BufferedReader(InputStreamReader(process.errorStream))
+        val output = StringBuilder()
+        val stdErr = StringBuilder()
+        var line: String?
+
+        // Capture standard output
+        while (reader.readLine().also { line = it } != null) {
+            output.append(line).append("\n")
+        }
+
+        // Capture standard error
+        while (stdErrReader.readLine().also { line = it } != null) {
+            stdErr.append(line).append("\n")
+        }
+        val exitCode = process.waitFor()
+        if (exitCode == 0) {
+            println("code 0")
+            output.toString()
+        }
+        else {
+            "Command failed with exit code $exitCode, and error message $stdErr"
+        }
+    }
+    catch (e: Exception) {
+        e.printStackTrace()
+        e.message.orEmpty()
+    }
 }
